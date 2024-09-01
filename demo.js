c82202af3ac1decb01387c4344ea19fafea399cd
SadeshWijekoon <10898706@students.plymouth.ac.uk>
2024-09-01 16:06:36 +0530

    simple rest api

--- demo.js
+++ demo.js
@@ -1,20 +1,76 @@
-// var http = require('http');
-
-// http.createServer(function(req, res) {
-//     res.write("Hello");
-//     res.end();
-// }).listen(8000);
-
 const express = require('express');
+
 const app = express();
+app.use(express.json())
+const port = 3000;
 
-// Middleware for parsing JSON and URL-encoded data
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+let movies = [
+  {
+    id:'1',
+    title :'Inception',
+    director:'Christoper Nolen',
+    release_date:'2016-07-16',
+  },
+  {
+    id:'2',
+    title :'The Irishman',
+    director:'Martin',
+    release_date:'2019-09-27',
+  },
+]
 
-// Use the correct route and ensure you are importing a router
-app.use('/api/users/', require('./Routes/api/users'));
+// get the movie
+ app.get('/movie',(req,res)=>{
+    res.json(movies)
+ })
 
-app.listen(4000, () => {
-    console.log("Server is Started 4000");
-});
+ // get the movie by filtering 
+ app.get('/movie/:id',(req,res)=>{
+    const found = movies.find(movie=>movie.id===req.params.id)
+    if (found){
+        res.json(found)
+    } else{
+        res.sendStatus(400)
+    }
+ })
+  // add the movie
+  app.post('/movie',(req,res)=>{
+   
+   const update ={...req.body, id:String(movies.length+1) }
+   movies.push(update)
+   res.json(update)
+  })
+
+  // update a movie
+  app.put('/movie/:id',(req,res)=>{
+    const found = movies.some(movie=>movie.id===req.params.id)
+    if (found){
+        movies.forEach(movie=>{
+            if(movie.id===req.params.id){
+                movie.title=req.body.title,
+                movie.director=req.body.director,
+                movie.release_date=req.body.release_date
+            }else{
+                movie
+            }
+        })
+        res.json(movies)
+    }else{
+        res.sendStatus(400)
+    }
+
+    // delete a movie
+    app.delete('/movie/:id',(req,res)=>{
+        const found = movies.findIndex(movie=>movie.id===req.params.id)
+        if(found){
+            movies.splice(found,1)
+            res.json(movies)
+        }
+    })
+  })
+
+ app.listen(port,()=>{
+    console.log(`server is started on ${port}`);
+    
+ })
+
